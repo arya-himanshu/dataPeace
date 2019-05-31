f743d703229bc6efd93d28819130ef6b138e140c
arya-himanshu <aryahimanshu911>
2019-05-31 18:04:06 +0530

    latest commity

--- src/app/UserContainer/user/user.component.ts
+++ src/app/UserContainer/user/user.component.ts
@@ -15,6 +15,8 @@ export class UserComponent implements OnInit {
   pageIndexs: Array<number> = new Array();
   userLength: number;
   keyword: string;
+  perPage = 5;
+  currentSortTpe: string = "id";
   ngOnInit() {
     this.getUses();
   }
@@ -27,11 +29,13 @@ export class UserComponent implements OnInit {
     this.users = response;
     this.globalUsers = response;
     this.userLength = response.length;
-    this.chnageIndex(1);
+    this.chnageIndex(this.globalUsers, this.userLength, this.perPage, 1);
   }
 
-  chnageIndex(index) {
-    this.paginate(this.globalUsers, this.userLength, 5, index);
+  chnageIndex(user, length, perPage, index) {
+    this.paginate(user, length, perPage, index);
+    console.log(this.currentSortTpe);
+    this.sorting(this.currentSortTpe);
   }
 
   paginate(
@@ -41,6 +45,9 @@ export class UserComponent implements OnInit {
     currentIndex: number
   ) {
     let paginateNo: number;
+    let localUsers: Array<User> = new Array();
+    localUsers = users;
+
     paginateNo = userLength / perPage;
     this.users = [];
     this.pageIndexs = [];
@@ -50,7 +57,7 @@ export class UserComponent implements OnInit {
     for (let i = 1; i <= paginateNo; i++) {
       this.pageIndexs.push(i);
     }
-    for (let user of this.globalUsers.slice(
+    for (let user of localUsers.slice(
       perPage * currentIndex - perPage,
       perPage * currentIndex
     )) {
@@ -59,7 +66,123 @@ export class UserComponent implements OnInit {
   }
 
   searchUser(keyword: string) {
-    console.log(keyword);
-    console.log(this.globalUsers.find(e => e.first_name === keyword)); // {foo: 'bar'} (first match)
+    if (
+      this.globalUsers != undefined &&
+      this.globalUsers.length > 0 &&
+      keyword != undefined &&
+      keyword.length > 0
+    ) {
+      this.users = [];
+      for (let user of this.globalUsers) {
+        if (user.first_name.toLowerCase().search(keyword.toLowerCase()) > -1) {
+          this.users.push(user);
+        }
+      }
+      this.globalUsers = this.users;
+      this.userLength = this.users.length;
+      this.paginate(this.users, this.userLength, this.perPage, 1);
+    } else {
+      this.getUses();
+    }
+  }
+
+  sorting(type) {
+    this.currentSortTpe = type;
+    this.users = this.users.sort((n1, n2) => {
+      switch (type) {
+        case "id":
+          if (n1.id > n2.id) {
+            return 1;
+          }
+
+          if (n1.id < n2.id) {
+            return -1;
+          }
+          return 0;
+        case "first_name":
+          if (n1.first_name > n2.first_name) {
+            return 1;
+          }
+
+          if (n1.first_name < n2.first_name) {
+            return -1;
+          }
+          return 0;
+        case "last_name":
+          if (n1.last_name > n2.last_name) {
+            return 1;
+          }
+
+          if (n1.last_name < n2.last_name) {
+            return -1;
+          }
+          return 0;
+        case "company_name":
+          if (n1.company_name > n2.company_name) {
+            return 1;
+          }
+
+          if (n1.company_name < n2.company_name) {
+            return -1;
+          }
+          return 0;
+        case "city":
+          if (n1.city > n2.city) {
+            return 1;
+          }
+
+          if (n1.city < n2.city) {
+            return -1;
+          }
+          return 0;
+        case "state":
+          if (n1.state > n2.state) {
+            return 1;
+          }
+
+          if (n1.state < n2.state) {
+            return -1;
+          }
+          return 0;
+        case "zip":
+          if (n1.zip > n2.zip) {
+            return 1;
+          }
+
+          if (n1.zip < n2.zip) {
+            return -1;
+          }
+          return 0;
+        case "email":
+          if (n1.email > n2.email) {
+            return 1;
+          }
+
+          if (n1.email < n2.email) {
+            return -1;
+          }
+          return 0;
+        case "web":
+          if (n1.web > n2.web) {
+            return 1;
+          }
+
+          if (n1.web < n2.web) {
+            return -1;
+          }
+          return 0;
+        case "age":
+          if (n1.age > n2.age) {
+            return 1;
+          }
+
+          if (n1.age < n2.age) {
+            return -1;
+          }
+          return 0;
+        default:
+        // code block
+      }
+    });
   }
 }
